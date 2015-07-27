fdd2ab2d62189e82b5e5e43c70f6a2655a5faa85
dongxu <huang@wandoujia.com>
2015-07-27 16:53:51 +0800

    add del implement

--- example/main.go
+++ example/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ngaut/log"
 	"github.com/pingcap/go-themis"
 )
 
 func main() {
+	log.SetLevelByString("error")
 	c, err := themis.NewClient([]string{"localhost"}, "/hbase")
 	if err != nil {
 		log.Fatal(err)
@@ -17,13 +20,14 @@ func main() {
 	}
 	log.Info(result, result.Columns["cf:v"].Value, result.Columns["cf:v"].Timestamp.Unix())
 
-	put := themis.CreateNewPut([]byte("hello"))
-	put.AddValue([]byte("cf"), []byte("v"), []byte("new value"))
-	b, err := c.Put("t1", put)
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i < 10000; i++ {
+		put := themis.CreateNewPut([]byte(fmt.Sprintf("hello_1_%d", i)))
+		put.AddValue([]byte("cf"), []byte("v"), []byte("new value"))
+		_, err := c.Put("t1", put)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	log.Info(b)
 
 	get = themis.CreateNewGet([]byte("hello"))
 	result, err = c.Get("t1", get)
