65314dfb7961a356ece7854b62d2ac35af1181ab
disksing <i@disksing.com>
2015-11-19 10:47:39 +0800

    *: address comments

--- oracle/oracles/remote.go
+++ oracle/oracles/remote.go
@@ -5,20 +5,30 @@ import (
 	"github.com/ngaut/tso/client"
 )
 
+const remoteRetryTimes = 3
+
+// RemoteOracle is an oracle that use a remote data source.
+// Refer https://github.com/ngaut/tso for more details.
 type RemoteOracle struct {
 	c *client.Client
 }
 
+// NewRemoteOracle creates a RemoteOracle, addr should be formatted as 'host:port'.
 func NewRemoteOracle(addr string) *RemoteOracle {
 	return &RemoteOracle{
 		c: client.NewClient(&client.Conf{ServerAddr: addr}),
 	}
 }
 
+// GetTimestamp gets timestamp from remote data source.
 func (t *RemoteOracle) GetTimestamp() (uint64, error) {
-	ts, err := t.c.GoGetTimestamp().GetTS()
-	if err != nil {
-		return 0, errors.Trace(err)
+	var err error
+	for i := 0; i < remoteRetryTimes; i++ {
+		ts, e := t.c.GoGetTimestamp().GetTS()
+		if e == nil {
+			return uint64((ts.Physical << 18) + ts.Logical), nil
+		}
+		err = errors.Trace(e)
 	}
-	return uint64((ts.Physical << 18) + ts.Logical), nil
+	return 0, err
 }
